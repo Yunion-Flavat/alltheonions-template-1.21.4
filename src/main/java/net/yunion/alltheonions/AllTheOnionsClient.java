5492a5b9cdd4e5f65470a98fd1e342c53e889a62
Yunion-Flavat <jakeaman2001@gmail.com>
2025-01-31 18:57:13 -0600

    Innital Commit

---
+++ src/main/java/net/yunion/alltheonions/AllTheOnionsClient.java
@@ -0,0 +1,10 @@
+package net.yunion.alltheonions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AllTheOnionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/yunion/alltheonions/AllTheOnionsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.yunion.alltheonions;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AllTheOnionsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
